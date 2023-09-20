ab09be407b35b47d4830b0d09bb6e16291e727e2
Nico <lopeznnr@gmail.com>
2023-09-20 14:30:39 -0300

    install mongoose and conection with database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const cookieParser=require('cookie-parser')
 const authRouter=require('./routes/authRoutes')
+const mongoose = require('mongoose')
 
 const app =express()
 
@@ -11,6 +12,12 @@ app.use(cookieParser())
 // motor de plantilla
 app.set('view engine','ejs')
 
+// conection database MONGO DB ATLAS
+
+const dbUrl = 'mongodb+srv://lopeznnr:<password>@cluster0.7cmpogq.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbUrl)
+.then((result)=>app.listen(3200))
+.catch((error)=>console.log(error))
 
 app.get('/', (req,res)=>{
     res.render('home')
@@ -18,6 +25,6 @@ app.get('/', (req,res)=>{
 
 app.use(authRouter)
 
-app.listen(3200, () => {
-    console.log('servidor ejecutandose')
-})
+// app.listen(3200, () => {
+//     console.log('servidor ejecutandose')
+// })
