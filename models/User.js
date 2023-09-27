f8d76e00a5ccce9e542550c28a97981e052c043b
Nico <lopeznnr@gmail.com>
2023-09-27 08:16:43 -0300

    create User and exports module

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require ('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    }
+})
+
+const User = mongoose.model('usuarios', userSchema)
+module.exports= User
