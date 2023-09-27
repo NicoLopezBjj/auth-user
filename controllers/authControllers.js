0e9c1220547220a972c4e5993318cc6eb6486a99
Nico <lopeznnr@gmail.com>
2023-09-27 08:22:32 -0300

    import User in authControllers and init logic of signup post(try/catch)

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,14 @@
+const User = require ('../models/User')
+
 module.exports.signup_post=(req,res)=>{
     // destructuring
     const {email,password}=req.body
+    try{
+        const user = User.create({email,password})
+        res.status(201).json()
+    }catch(error){
+        console.log(error)
+    }
 }
 
 module.exports.signup_get=(req,res)=>{
